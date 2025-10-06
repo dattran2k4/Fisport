05be01b5bdb3aee9fc68ecbea76f0936795be2a9
dattran2k4 <tranquocthanhdat@gmail.com>
2025-10-06 23:54:12 +0700

    edit banner

--- src/main/java/com/Fisport/controller/HomeController.java
+++ src/main/java/com/Fisport/controller/HomeController.java
@@ -1,12 +1,22 @@
 package com.Fisport.controller;
 
+import com.Fisport.dto.response.FieldTypeResponse;
+import com.Fisport.service.FieldTypeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@Validated
 @Controller
 public class HomeController {
+
+    private final FieldTypeService fieldTypeService;
 
     @GetMapping("/")
     public RedirectView root() {
@@ -15,6 +25,8 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home(Model model) {
+        List<FieldTypeResponse> list = fieldTypeService.findAll();
+        model.addAttribute("fieldTypes", list);
         return "web/index";
     }
 
