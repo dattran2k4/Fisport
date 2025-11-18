0dc0a51f95d1d11697c861fd0ff488bb112f0d9c
HungNT <enterfenrye@gmail.com>
2025-11-19 05:26:04 +0700

    Update src/main/java/com/fisport/repository/FieldServiceItemRepository.java

--- src/main/java/com/fisport/repository/FieldServiceItemRepository.java
+++ src/main/java/com/fisport/repository/FieldServiceItemRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FieldServiceItemRepository extends JpaRepository<FieldServiceItem, Long>, JpaSpecificationExecutor<FieldServiceItem> {
@@ -29,8 +30,29 @@ public interface FieldServiceItemRepository extends JpaRepository<FieldServiceIt
             "ORDER BY f.name, s.name, si.name")
     List<FieldServiceItem> findAllByOwnerId(Long ownerId);
 
+    @Query("SELECT fsi FROM FieldServiceItem fsi " +
+            "JOIN FETCH fsi.serviceItem si " +
+            "JOIN FETCH si.service s " +
+            "WHERE fsi.field.id = :fieldId")
     List<FieldServiceItem> findByFieldId(Long fieldId);
 
     void deleteAllByField_Id(Long fieldId);
+
+    @Query("SELECT fsi FROM FieldServiceItem fsi " +
+            "JOIN FETCH fsi.serviceItem si " +
+            "JOIN FETCH si.service s " +
+            "WHERE fsi.field.id = :fieldId AND si.service.id = :serviceId")
+    List<FieldServiceItem> findByFieldIdAndServiceId(Long fieldId, Long serviceId);
+
+    @Query("SELECT fsi FROM FieldServiceItem fsi " +
+            "JOIN FETCH fsi.serviceItem si " +
+            "JOIN FETCH si.service s " +
+            "WHERE fsi.field.id = :fieldId AND si.service.id = :serviceId AND fsi.status = 'ACTIVE'")
+    List<FieldServiceItem> findActiveByFieldIdAndServiceId(Long fieldId, Long serviceId);
+
+    @Query("SELECT fsi FROM FieldServiceItem fsi " +
+            "JOIN FETCH fsi.serviceItem si " +
+            "WHERE fsi.field.id = :fieldId AND fsi.serviceItem.id = :serviceItemId")
+    Optional<FieldServiceItem> findByFieldIdAndServiceItemId(Long fieldId, Long serviceItemId);
 
 }
