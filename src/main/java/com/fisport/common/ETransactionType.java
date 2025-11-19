eaeec1ea61e106b312fa19c4e85e9a5ab75fdca5
dattran2k4 <tranquocthanhdat@gmail.com>
2025-11-19 07:00:00 +0700

    update

--- src/main/java/com/fisport/common/ETransactionType.java
+++ src/main/java/com/fisport/common/ETransactionType.java
@@ -1,14 +1,22 @@
 package com.fisport.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum ETransactionType {
     @JsonProperty("topup")
-    TOUP,
+    TOUP("Nạp tiền"),
     @JsonProperty("payment")
-    PAYMENT,
+    PAYMENT("Thanh toán"),
     @JsonProperty("refund")
-    REFUND,
+    REFUND("Hoàn tiền"),
     @JsonProperty("received")
-    RECEIVED
+    RECEIVED("Nhận tiền"),
+    @JsonProperty("bonus")
+    BONUS("Thưởng bonus");
+
+    private final String value;
 }
